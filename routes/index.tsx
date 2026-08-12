d5f447de68a044b1bb2a1c679e127a6adf5c47d1
Odil005 <bekpolatovsoatmurod6@gmail.com>
2026-08-12 19:47:21 +0500

    Create index.tsx

---
+++ routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-4">
+      <h1>Ahmad Akademiyasiga xush kelibsiz!</h1>
+    </div>
+  )
+}
